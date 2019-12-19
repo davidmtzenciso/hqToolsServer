b2506542ee9c30428576b0fdf096f6c6bff1f600
david.martinez <david.martinez@host.docker.internal>
2019-12-19 16:47:06 -0600

    added model ticket class

---
+++ src/main/java/com/healthsparq/app/model/Ticket.java
@@ -0,0 +1,49 @@
+package com.healthsparq.app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ticket {
+
+	private String title;
+	private String type;
+	private int number;
+	private String label;
+	private String customer;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getCustomer() {
+		return customer;
+	}
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+	
+	
+	
+	
+}
