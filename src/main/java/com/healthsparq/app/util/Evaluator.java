67286d95b706e11c70d7f9b6c19e0a41dc279477
david.martinez <david.martinez@host.docker.internal>
2019-12-19 18:01:29 -0600

    added unit test for release date validation

--- src/main/java/com/healthsparq/app/util/Evaluator.java
+++ src/main/java/com/healthsparq/app/util/Evaluator.java
@@ -1,6 +1,9 @@
 package com.healthsparq.app.util;
 
 import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,12 +15,26 @@ public class Evaluator {
 	@Autowired
 	@Qualifier("schemaList")
 	private List<String> schemas;
-
+	
+	private static final String DATE_FORMAT = "yyyy/MM/dd";
+	private static final String DATE_REGEX = "[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}";
+	
 	public Boolean isExistingSchema(String name) {
 		return schemas.contains(name.toLowerCase());
 	}
 	
-	public Boolean isValidReleaseDate(String date) {
-		return null;
+	public Boolean isValidReleaseDate(String text) {
+		try {
+			if(text.matches(DATE_REGEX)) {
+				var dateFormat = new SimpleDateFormat(DATE_FORMAT);
+				var date = dateFormat.parse(text);
+				return date != null;
+			} else {
+				return false;
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
--- src/test/java/com/healthsparq/app/util/unit/evaluator/ValidateReleaseDate.java
+++ src/test/java/com/healthsparq/app/util/unit/evaluator/ValidateReleaseDate.java
@@ -14,10 +14,50 @@ public class ValidateReleaseDate {
 	private Evaluator evaluator;
 	
 	@Test
-	public void given_date_has_wrong_format_when_is_validated_it_should_fail() {
+	public void given_date_has_wrong_order_when_is_validated_it_should_fail() {
+		
+		//given date has wrong order
+		final String date = "2019-13-12";
+		
+		//when is validated
+		final Boolean isValid = evaluator.isValidReleaseDate(date);
+		
+		//then it should fail
+		Assertions.assertFalse(isValid);
+	}
+	
+	@Test
+	public void given_date_has_wrong_delimiters_when_is_validated_it_should_fail() {
 		
 		//given date has wrong format
-		final String date = "2019-13-12";
+		final String date = "2019/12/12";
+		
+		//when is validated
+		final Boolean isValid = evaluator.isValidReleaseDate(date);
+		
+		//then it should fail
+		Assertions.assertFalse(isValid);
+	}
+	
+	@Test
+	public void given_date_has_wrong_values_when_validated_it_should_fail() {
+		
+		//given date has wrong format
+		final String date = "2020-13-23";
+		
+		//when is validated
+		final Boolean isValid = evaluator.isValidReleaseDate(date);
+		
+		//then it should fail
+		Assertions.assertFalse(isValid);
+	}
+	
+
+	@Test
+	public void given_date_follows_all_the_rules_when_validated_it_should_pass() {
+		
+		//given date has wrong format
+		final String date = "2020-12-1";
 		
 		//when is validated
 		final Boolean isValid = evaluator.isValidReleaseDate(date);
