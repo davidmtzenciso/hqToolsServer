60ad2a5972ee976f1e28eeb29e1f675145fc307a
david.martinez <david.martinez@SAO-LT-191014B.ad.unosquare.com>
2020-01-01 14:23:10 -0600

    added model classes for testing SQLTranslator

---
+++ src/test/java/com/healthsparq/app/util/unit/sqltranslator/model/ClassWithFieldWithoutAnnotation.java
@@ -0,0 +1,18 @@
+package com.healthsparq.app.util.unit.sqltranslator.model;
+
+import javax.persistence.Table;
+
+@Table(name="TABLE_NAME")
+
+public class ClassWithFieldWithoutAnnotation {
+
+	private Integer field1;
+
+	public Integer getField1() {
+		return field1;
+	}
+
+	public void setField1(Integer field1) {
+		this.field1 = field1;
+	}
+}
---
+++ src/test/java/com/healthsparq/app/util/unit/sqltranslator/model/ClassWithInvalidFieldType.java
@@ -0,0 +1,19 @@
+package com.healthsparq.app.util.unit.sqltranslator.model;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Table(name="TABLE_NAME")
+public class ClassWithInvalidFieldType {
+	
+	@Column(name="COLUMN_1")
+	private Double field1;
+
+	public Double getField1() {
+		return field1;
+	}
+
+	public void setField1(Double field1) {
+		this.field1 = field1;
+	}
+}
